b8c93777b5fa9958adb8a1dc3d3727a03563f604
digitsmart <69549818+digitsmart@users.noreply.github.com>
2021-06-24 12:12:22 -0700

    Add files via upload

--- Formation/salle/salle.js
+++ Formation/salle/salle.js
@@ -9,3 +9,32 @@ function aller() {
       home.style.display = "none";
     }
   }
+
+  function first() {
+    var home = document.getElementById("home");
+    var first = document.getElementById("first");
+    var second = document.getElementById("second");
+    if (first.style.display === "block") {
+      home.style.display = "none";
+      first.style.display = "none";
+      second.style.display = "block";
+    } else {
+      home.style.display = "none";
+      first.style.display = "none";
+      second.style.display = "block";
+    }
+  }
+  function pre() {
+    var home = document.getElementById("home");
+    var first = document.getElementById("first");
+    var second = document.getElementById("second");
+    if (first.style.display === "none") {
+      home.style.display = "none";
+      first.style.display = "block";
+      second.style.display = "none";
+    } else {
+      home.style.display = "none";
+      first.style.display = "block";
+      second.style.display = "none";
+    }
+  }
