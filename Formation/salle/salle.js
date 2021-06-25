3986d293f901499063a32eec5f47328a287c5bf1
digitsmart <69549818+digitsmart@users.noreply.github.com>
2021-06-25 01:14:02 -0700

    Add files via upload

--- Formation/salle/salle.js
+++ Formation/salle/salle.js
@@ -1,16 +1,19 @@
 function aller() {
     var home = document.getElementById("home");
     var first = document.getElementById("first");
+    var navig = document.getElementById('navig');
     if (home.style.display === "block") {
       home.style.display = "none";
+      navig.style.display = "block";
       first.style.display = "block";
     } else {
       first.style.display = "block";
       home.style.display = "none";
+      navig.style.display = "block";
     }
   }
 
-  function first() {
+  function suiv() {
     var home = document.getElementById("home");
     var first = document.getElementById("first");
     var second = document.getElementById("second");
@@ -28,13 +31,16 @@ function aller() {
     var home = document.getElementById("home");
     var first = document.getElementById("first");
     var second = document.getElementById("second");
-    if (first.style.display === "none") {
+    var navig = document.getElementById("navig");
+    if (first.style.display === "block") {
+      home.style.display = "block";
+      first.style.display = "none";
+      navig.style.display = "none";
+      second.style.display = "none";
+    } else if (second.style.display === "block") {
       home.style.display = "none";
       first.style.display = "block";
       second.style.display = "none";
-    } else {
-      home.style.display = "none";
-      first.style.display = "block";
-      second.style.display = "none";
+
     }
   }
