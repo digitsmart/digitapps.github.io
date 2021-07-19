98e7f8c4274877fde40ef801f74c3ac98a186fd7
digitsmart <69549818+digitsmart@users.noreply.github.com>
2021-07-19 09:34:10 -0700

    Add files via upload

--- shope/js/shopepaiment.js
+++ shope/js/shopepaiment.js
@@ -1,37 +1,4 @@
-
-//........  faire apparaitre les details du shope.....................
-function card() {
-  var passWindows = document.getElementById("passWindows");
-  var lien = document.getElementById("lien");
-  var utilisateur =document.getElementById('utilisateur');
-  var sectioncard = document.getElementById('sectioncard');
-  if (sectioncard.style.display === "block") {
-    sectioncard.style.display = "none";
-    passWindows.style.display = "none";
-    lien.style.display = "none";
-    utilisateur.style.display = "none";
-  } else {
-    passWindows.style.display = "none";
-    lien.style.display = "none";
-    utilisateur.style.display = "none";
-    sectioncard.style.display = "block";
-    sectioncard.style.display = "block";
-  }
-}
 //........  crack mot de pass windows.....................
-function acheterPass() {
-  var passWindows = document.getElementById("passWindows");
-  var formPass = document.getElementById('formPass');
-  if (passWindows.style.display === "block") {
-    formPass.style.display = "block";
-    passWindows.style.display = "none";
-  } else {
-    formPass.style.display = "block";
-    passWindows.style.display = "none";
-  }
-}
-
-//........  html  et css.....................
 function acheterPass() {
   var passWindows = document.getElementById("passWindows");
   var formPass = document.getElementById('formPass');
