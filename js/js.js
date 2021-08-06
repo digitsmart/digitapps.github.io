12d7c405bd569d0f73a16b7df5d55f25bd2f835a
digitsmart <69549818+digitsmart@users.noreply.github.com>
2021-08-06 16:42:58 -0700

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -35,6 +35,26 @@ var myIndex = 0;
           window.open(url, '_blank').focus();
       }
 
+      // video playliste de la page home
+      var video_player = document.getElementById("video_player"),
+    links = video_player.getElementsByTagName('a');
+    for (var i=0; i<links.length; i++) {
+      links[i].onclick = handler;
+}
+function handler(e) {
+	e.preventDefault();
+	videotarget = this.getAttribute("href");
+	filename = videotarget.substr(0, videotarget.lastIndexOf('.')) || videotarget;
+	video = document.querySelector("#video_player video");
+	video.removeAttribute("controls");
+	video.removeAttribute("poster");
+	source = document.querySelectorAll("#video_player video source");
+	source[0].src = filename + ".mp4";
+	source[1].src = filename + ".webm";
+	video.load();
+	video.play();    
+}
+
         // boutton bar en bas
         function home(){
           var v = document.getElementById('videoPage');
