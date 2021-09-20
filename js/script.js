38d704325382c27687f00b824e33ace594680b95
digitsmart <69549818+digitsmart@users.noreply.github.com>
2021-09-20 10:22:38 -0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -72,7 +72,25 @@ setTimeout(() => {
  // Your web app's Firebase configuration
 
  var codeV, nameV, emailV, numberV, adressV, infosV;
+ setInterval(()=>{
 
+  let date = new Date(),
+  hour = date.getHours(),
+  min = date.getMinutes(),
+  sec = date.getSeconds();
+
+  let d;
+  d = hour < 12 ? "AM" : "PM"; //if hour is smaller than 12, than its value will be AM else its value will be pm
+  hour = hour > 12 ? hour - 12 : hour; //if hour value is greater than 12 than 12 will subtracted ( by doing this we will get value till 12 not 13,14 or 24 )
+  hour = hour == 0 ? hour = 12 : hour; // if hour value is  0 than it value will be 12
+
+  // adding 0 to the front of all the value if they will less than 10
+  hour = hour < 10 ? "0" + hour : hour;
+  min = min < 10 ? "0" + min : min;
+  sec = sec < 10 ? "0" + sec : sec;
+  
+
+}, 1000); // 1000 milliseconds = 1s
  function enregistrer() {
       nameV = document.getElementById('name').value;
       codeV = document.getElementById('code').value;
@@ -93,7 +111,7 @@ setTimeout(() => {
    }else if(adressV == ""){
     document.getElementById('message').innerHTML ="Votre adresse";
   }else{
-     firebase.database().ref('Cours google drive/' + nameV).set({
+     firebase.database().ref('formation/' + nameV).set({
          ANAME:  nameV,
          Email: emailV,
          NUMBER: (codeV) + numberV,
